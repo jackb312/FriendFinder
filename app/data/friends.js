4a509c391023f3fb0822746966fe755046889850
Jack Brettell <jack.brettell@gmail.com>
2019-10-20 20:56:00 -0700

    commiting initial code

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+var friends = [
+    {
+        name: "Sofia",
+        photo: "https://cdn.britannica.com/65/180565-050-4932FA9F/Sofia-Vergara-Colombian.jpg",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Tom",
+        photo: "https://www.biography.com/.image/t_share/MTQ0ODY2Mzc4OTI5Njc3Njg2/tom_brady_michael_loccisano_getty_images_609582044_profile.jpg",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Shakira",
+        photo: "https://www.shakira.com/uploaded/home_banner/47ced7e9ccf86af8dcd6ba4ca733dce4.png",
+        scores: [
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Russ",
+        photo: "https://www.biography.com/.image/t_share/MTQxMDMwOTgyNzk4MDI1OTEx/russell-wilson-gettyimages-576525424_1600jpg.jpg",
+        scores: [
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+];
+module.exports = friends;
