37f30d1a738396dd94b12a30c399bd18cf5e8265
Jack Brettell <jack.brettell@gmail.com>
2019-10-20 21:44:52 -0700

    initial heroku commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,9 @@
 var path = require("path");
 module.exports = function(app){
     app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/survey"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
